a874dec9e84ccef9b36e40235e7adbc1da365812
Jayavelu <bhanuprakash@BhanuPrakash-Mac-Mini-2.local>
2014-08-30 14:48:22 +0530

    Added Some UserInterface and prints "Hello World" message while clicking the button...

---
+++ MYAPP/MYAPP/View2.h
@@ -0,0 +1,18 @@
+//
+//  View2.h
+//  MYAPP
+//
+//  Created by BhanuPrakash on 8/30/14.
+//  Copyright (c) 2014 BhanuPrakash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface View2 : UIViewController
+{
+    IBOutlet UILabel *hellowworld;
+    
+}
+-(IBAction)Button:(id)sender;
+
+@end
---
+++ MYAPP/MYAPP/View2.m
@@ -0,0 +1,39 @@
+//
+//  View2.m
+//  MYAPP
+//
+//  Created by BhanuPrakash on 8/30/14.
+//  Copyright (c) 2014 BhanuPrakash. All rights reserved.
+//
+
+#import "View2.h"
+
+@interface View2 ()
+
+@end
+
+@implementation View2
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"Hello World");
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
